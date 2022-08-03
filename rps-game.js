a6f47c4114112c998971aa39e9de9834820c23ad
chuchuluco <oscarfdez05@gmail.com>
2022-08-03 18:22:05 -0500

    Add playRound conditions

--- rps-game.js
+++ rps-game.js
@@ -5,7 +5,7 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let str = prompt("What's your choice?")
+    let str = prompt("What's your choice?(rock, paper, scissors)")
     let choice = str.toLowerCase()
     return choice;
 }
@@ -14,9 +14,21 @@ const computerSelection = getComputerChoice();
 const playerSelection = getPlayerChoice();
 
 function playRound(playerSelection, computerSelection) {
-    if (player == computer) {
+    if (playerSelection == computerSelection) {
         return "Wow! It's a Tie."
-    } else if (player) {}
+    } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You Win! Scissors beats paper"
+    } 
 }
 
 function game() {
@@ -26,5 +38,3 @@ function game() {
      }
     
 }
-
-getPlayerChoice()
