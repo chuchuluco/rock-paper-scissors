06935e4a586b3aad03a4be90812366defe8f0638
chuchuluco <oscarfdez05@gmail.com>
2022-08-03 16:08:32 -0500

    add player function and round function
    added functions for the player and the game round, not yet added
    full round options

--- rps-game.js
+++ rps-game.js
@@ -1,7 +1,30 @@
-
-
 function getComputerChoice() {
-    let choices = [rock, paper, scissors]
-    let index = Math.floor(Math.random() * choices.length);
-    return choices[index];
-} 
+    let array = ["rock", "paper", "scissors"]
+    let index = Math.floor(Math.random() * array.length);
+    return array[index];
+}
+
+function getPlayerChoice() {
+    let str = prompt("What's your choice?")
+    let choice = str.toLowerCase()
+    return choice;
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = getPlayerChoice();
+
+function playRound(playerSelection, computerSelection) {
+    if (player == computer) {
+        return "Wow! It's a Tie."
+    } else if (player) {}
+}
+
+function game() {
+    
+    for (let i = 0; i < 5; i++) {
+        playRound()
+     }
+    
+}
+
+getPlayerChoice()
