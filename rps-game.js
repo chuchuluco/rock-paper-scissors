c33cc9d7833c4f91588cb4517269807efe522926
chuchuluco <oscarfdez05@gmail.com>
2022-08-13 13:07:54 -0500

    Add styles and results div
    added some color and font to the style, worked on the result div
    making it work with the gameRound function, tweak the margins and
    the sizes of the buttons container and the container

--- rps-game.js
+++ rps-game.js
@@ -13,30 +13,32 @@ function getPlayerChoice() {
 let playerScore = 0;
 let computerScore = 0;
 let roundsPlayed = 0;
+let gameState = "";
+let blue = 'rgba(20, 5, 227, 0.834)'
+let greenMunsell = 'hsla(163, 100%, 33%, 1)'
+let lavenderWeb = 'rgba(24, 255, 247, 0.864'
+let celadonBlue = 'rgb(52, 203, 254)'
+let carmine = 'hsla(348, 73%, 49%, 1)'
 
 
-
-
-function game() {
-    const computerSelection = getComputerChoice();
-    const playerSelection = getPlayerChoice();
-    gameState = playRound(playerSelection, computerSelection)
-    console.log(gameState)
-    return alert(gameState)
-}
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     if(button.id == "rock") {
-        console.log(gameRound("rock"))
+        result.textContent = gameRound("rock")
     } else if(button.id == "paper") {
-        console.log(gameRound("paper"))
+        result.textContent = gameRound("paper")
     } else if(button.id == "scissors") {
-        console.log(gameRound("scissors"))
+        result.textContent = gameRound("scissors")
     }
   });
 });
+
+const result = document.querySelector("#result");
+
+const container = document.querySelector('.container')
+
 
 
 function gameRound(playerSelect) {
@@ -45,21 +47,27 @@ function gameRound(playerSelect) {
         return "Wow! It's a Tie."
     } else if (playerSelect == "rock" && computer == "scissors") {
         playerScore ++
+        container.style.backgroundColor = greenMunsell;
         return "You Win! Rock beats Scissors" 
     } else if (playerSelect == "rock" && computer == "paper") {
         computerScore ++
+        container.style.backgroundColor = carmine;
         return "You Lose! Paper beats Rock" 
     } else if (playerSelect == "paper" && computer == "rock") {
         playerScore ++
+        container.style.backgroundColor = greenMunsell;
         return "You Win! Paper beats Rock"
     } else if (playerSelect == "paper" && computer == "scissors") {
         computerScore ++
+        container.style.backgroundColor = carmine;
         return "You Lose! Scissors beats Paper"
     } else if (playerSelect == "scissors" && computer == "rock") {
         computerScore ++
+        container.style.backgroundColor = carmine;
         return "You Lose! Rock beats Scissors"
     } else if (playerSelect == "scissors" && computer == "paper") {
         playerScore ++
+        container.style.backgroundColor = greenMunsell;
         return "You Win! Scissors beats paper"
     }
 };
